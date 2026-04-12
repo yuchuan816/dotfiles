aec3382408081a1730f4031ccb1e4a4f01f441c9
Tom <liuyuchuan816@live.com>
2026-04-12 20:51:37 +0800

    update

--- my-custom-commands.plugin.zsh
+++ my-custom-commands.plugin.zsh
@@ -4,24 +4,26 @@
 () {
     # 系统标识抽象 (使用 readonly local 锁定，兼容版本更新)
     local -r _OS_PLATFORM="${OSTYPE%%[0-9]*}"
-    local -r _MAC_TYPE="darwin"
-    local -r _LINUX_TYPE="linux-gnu"
-    
+
+    is_linux() { [[ "$_OS_PLATFORM" == "linux-gnu"  ]]; }
+    is_mac()   { [[ "$_OS_PLATFORM" == "darwin"  ]]; }
+    is_wsl()   { grep -qi "microsoft" /proc/version 2>/dev/null; }
+    is_nas()   { [[ "$(hostname)" == "nas-home" ]]; }
+
     #######################################
     # 代理
     #######################################
-    local -r _PORT="7897"
+    local _PORT="7897"
     local _HOST_IP=""
 
-    # 核心探测逻辑
-    if [[ "$_OS_PLATFORM" == "$_MAC_TYPE" ]]; then
-        _HOST_IP="127.0.0.1"
-    elif [[ "$_OS_PLATFORM" == "$_LINUX_TYPE" ]]; then
-        # 仅通过 ip route 获取
+    # 探测逻辑
+    if is_linux && is_nas; then
+        _HOST_IP="home.server"
+        _PORT="20171"
+    elif is_linux && is_wsl; then
         _HOST_IP=$(ip route | grep default | awk '{print $3}')
     else
-        echo "【错误】未识别的操作系统类型: $OSTYPE"
-        return 1
+        _HOST_IP="127.0.0.1"
     fi
 
     # 严格断路检查 (Fail-Fast)
@@ -32,13 +34,14 @@
 
     # 导出全局变量供 Alias 使用
     export PROXY_HOST_IP="$_HOST_IP"
+    export PROXY_PORT="$_PORT"
 
     # 定义 Alias
-    alias setp="export https_proxy=\"http://\$PROXY_HOST_IP:$_PORT\"; \
-                export http_proxy=\"http://\$PROXY_HOST_IP:$_PORT\"; \
-                export all_proxy=\"socks5://\$PROXY_HOST_IP:$_PORT\"; \
-                echo \"代理开启 -> \$PROXY_HOST_IP:$_PORT\"; \
-                curl -I https://www.google.com --connect-timeout 2"
+    alias setp='export https_proxy="http://${PROXY_HOST_IP}:${PROXY_PORT}"; \
+                export http_proxy="http://${PROXY_HOST_IP}:${PROXY_PORT}"; \
+                export all_proxy="socks5://${PROXY_HOST_IP}:${PROXY_PORT}"; \
+                echo "代理开启 -> ${PROXY_HOST_IP}:${PROXY_PORT}"; \
+                curl -I -s --connect-timeout 3 https://www.google.com > /dev/null && echo "✅ 网络连通正常" || echo "❌ 代理可能无法连接"'
 
     alias unsetp='unset https_proxy http_proxy all_proxy ALL_PROXY; echo "代理已关闭"'
     alias myip='curl -L ip.p3terx.com'
@@ -46,7 +49,7 @@
     #######################################
     # Mac Homebrew 镜像源修改
     #######################################
-    if [[ "$_OS_PLATFORM" == "$_MAC_TYPE" ]]; then
+    if is_mac; then
         # Homebrew 镜像源
         export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.ustc.edu.cn/brew.git"
         export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.ustc.edu.cn/homebrew-core.git"
@@ -59,15 +62,18 @@
     #######################################
     alias update-configs=" \
         echo '正在拉取 Vim 配置...'; \
-        curl -fLo ~/.vimrc \
-            https://raw.githubusercontent.com/yuchuan816/dotfiles/refs/heads/master/.vimrc; \
+        curl -fLo ~/.vimrc --create-dirs https://raw.githubusercontent.com/yuchuan816/dotfiles/refs/heads/master/.vimrc; \
         \
         echo '正在拉取 Zsh 插件自定义命令...'; \
-        mkdir -p ~/.oh-my-zsh/custom/plugins/my-custom-commands; \
-        curl -fLo ~/.oh-my-zsh/custom/plugins/my-custom-commands/my-custom-commands.plugin.zsh \
-            https://raw.githubusercontent.com/yuchuan816/dotfiles/refs/heads/master/my-custom-commands.plugin.zsh; \
-        \
-        echo '✅ 所有配置已更新！'; \
+        local tmp_file=\$(mktemp); \
+        if curl -fLo \"\$tmp_file\" https://raw.githubusercontent.com/yuchuan816/dotfiles/refs/heads/master/my-custom-commands.plugin.zsh; then \
+            mkdir -p ~/.oh-my-zsh/custom/plugins/my-custom-commands; \
+            \mv \"\$tmp_file\" ~/.oh-my-zsh/custom/plugins/my-custom-commands/my-custom-commands.plugin.zsh; \
+            echo '✅ 所有配置已更新！请执行 omz reload 生效。'; \
+        else \
+            echo '❌ 更新失败，请检查网络连接'; \
+            rm -f \"\$tmp_file\"; \
+        fi \
     "
 
     #######################################
