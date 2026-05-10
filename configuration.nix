b155f7d571fcd8d3ddf618cca6a3ef1b3e17c229
Liu Yuchuan <liuyuchuan816@live.com>
2026-05-10 17:32:31 +0800

    update

--- configuration.nix
+++ configuration.nix
@@ -8,7 +8,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.networkmanager.enable = true;
-  networking.hostName = "nixos"
+  networking.hostName = "nixos";
 
   time.timeZone = "Asia/Shanghai";
   i18n.defaultLocale = "en_US.UTF-8";
