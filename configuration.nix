fba1348919ef42a4edf906628e3738c3ab043fa1
Liu Yuchuan <liuyuchuan816@live.com>
2026-05-10 10:43:49 +0800

    update

--- configuration.nix
+++ configuration.nix
@@ -80,7 +80,6 @@
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
-    git
     htop
   ];
 
